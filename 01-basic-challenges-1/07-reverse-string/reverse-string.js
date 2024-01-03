67569c2b72f2f2980cbb9aa131492c59bd18f58e
Akshay7016 <asajgule@gmail.com>
2024-01-03 21:54:40 +0530

    Seventh problem solved

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,14 @@
-function reverseString() {}
+function reverseString(str) {
+    // 1st approach
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str.charAt(i);
+    }
+
+    return reversedStr;
+
+    // 2nd approach
+    // return Array.from(str).reverse().join("");
+}
 
 module.exports = reverseString;
