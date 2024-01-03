3c6e1754141752581543c12898bdfbcf48e98dce
Akshay7016 <asajgule@gmail.com>
2024-01-03 21:38:09 +0530

    Fifth problem solved

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(numbersArray) {
+    let max = numbersArray[0];
+
+    for (let i = 1; i < numbersArray.length; i++) {
+        if (max < numbersArray[i]) max = numbersArray[i]
+    };
+
+    return max;
+}
 
 module.exports = findMaxNumber;
