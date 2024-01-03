04fe67c8ccab9011dd76dbb3bc249272481acadf
Akshay7016 <asajgule@gmail.com>
2024-01-03 21:24:44 +0530

    Third problem solved

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,8 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    if (operator === "+") return a + b;
+    if (operator === "-") return a - b;
+    if (operator === "*") return a * b;
+    if (operator === "/") return a / b;
+}
 
 module.exports = calculator;
