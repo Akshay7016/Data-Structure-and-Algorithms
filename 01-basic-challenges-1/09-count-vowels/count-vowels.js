8b12340366ba134745d60a56be38729f8f6ae705
Akshay7016 <asajgule@gmail.com>
2024-01-05 20:53:33 +0530

    Ninth problem solved

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,13 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let count = 0;
+    const formattedStr = str.toLowerCase();
+
+    for (let i = 0; i < formattedStr.length; i++) {
+        if (vowels.includes(formattedStr.charAt(i))) count++;
+    }
+
+    return count;
+}
 
 module.exports = countVowels;
