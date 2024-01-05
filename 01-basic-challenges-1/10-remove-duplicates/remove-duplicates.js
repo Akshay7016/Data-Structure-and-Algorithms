7eb53b5a67eea56d351db1d3986ac71d5e727e9d
Akshay7016 <asajgule@gmail.com>
2024-01-05 20:56:46 +0530

    Tenth problem solved

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,13 @@
-function removeDuplicates() {}
+function removeDuplicates(array) {
+    const newArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (!newArray.includes(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
+}
 
 module.exports = removeDuplicates;
