29aff9b7cc7d64e46fb5baf323bed74d67bd7fc7
Akshay7016 <asajgule@gmail.com>
2024-04-26 21:17:39 +0530

    FizzBuzz problem solved

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,26 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const result = []
+
+    for (let i = 1; i <= num; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result.push("FizzBuzz");
+            continue;
+        }
+
+        if (i % 3 === 0) {
+            result.push("Fizz");
+            continue;
+        }
+
+        if (i % 5 === 0) {
+            result.push("Buzz");
+            continue;
+        }
+
+        result.push(i)
+    }
+
+    return result;
+}
 
 module.exports = fizzBuzzArray;
