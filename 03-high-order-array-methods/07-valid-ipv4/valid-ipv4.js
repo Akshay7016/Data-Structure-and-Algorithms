05156a750594c549111c1ce1789ff5b623293b3b
Akshay7016 <asajgule@gmail.com>
2024-04-26 21:21:12 +0530

    IPv4 problem solved

--- 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
+++ 03-high-order-array-methods/07-valid-ipv4/valid-ipv4.js
@@ -1,3 +1,16 @@
-const isValidIPv4 = () => {};
+const isValidIPv4 = (str) => {
+    const splittedArray = str.split(".");
+
+    if (splittedArray.length !== 4) {
+        return false;
+    };
+
+    for (let i = 0; i < 4; i++) {
+        if (+splittedArray[i] > 255 || splittedArray[i].startsWith('0')) return false
+    }
+
+    return true;
+
+};
 
 module.exports = isValidIPv4;
