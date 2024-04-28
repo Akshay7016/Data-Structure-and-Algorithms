7b292cfd45ed0a4181cd8a58425039d339d2b0dd
Akshay7016 <asajgule@gmail.com>
2024-04-28 12:17:28 +0530

    Display likes problem solved

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,11 @@
-function displayLikes() {}
+function displayLikes(arr) {
+    const arrayLength = arr.length;
+    if (arrayLength === 0) return 'no one likes this';
+    if (arrayLength === 1) return `${arr[0]} likes this`;
+    if (arrayLength === 2) return `${arr[0]} and ${arr[1]} like this`
+    if (arrayLength === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`
+
+    return `${arr[0]}, ${arr[1]} and ${arrayLength - 2} others like this`
+}
 
 module.exports = displayLikes;
