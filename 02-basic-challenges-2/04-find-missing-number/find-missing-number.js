8d385397528bbc8096aec0e57cb5dc15230f3395
Akshay7016 <asajgule@gmail.com>
2024-04-28 12:22:35 +0530

    missing number problem solved

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,9 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    const arrayLength = arr.length + 1;
+    const total = (arrayLength * (arrayLength + 1)) / 2;
+    const arraySum = arr.reduce((acc, ele) => acc + ele, 0)
+
+    return total - arraySum;
+}
 
 module.exports = findMissingNumber;
