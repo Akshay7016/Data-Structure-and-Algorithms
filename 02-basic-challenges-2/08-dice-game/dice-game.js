d955ac52029a9e9e49e4561d9f795fe7f712f1a7
Akshay7016 <asajgule@gmail.com>
2024-05-03 18:13:03 +0530

    roll dice problem solved

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,22 @@
-function diceGameSimulation() {}
+function diceGameSimulation(rollingCount) {
+    const results = [];
+    const winner = [7, 11];
+    const loser = [2, 3, 12];
+
+    for (let i = 0; i < rollingCount; i++) {
+        const dice1 = rollDice();
+        const dice2 = rollDice();
+        const sum = dice1 + dice2;
+        const result = winner.includes(sum) ? 'win' : loser.includes(sum) ? 'lose' : 'roll again';
+
+        results.push({ dice1, dice2, sum, result })
+    }
+
+    return results;
+}
+
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1
+}
 
 module.exports = diceGameSimulation;
