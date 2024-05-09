b0f7ede61e02aa17a65ac7ec8416818aa3fe4f52
Akshay7016 <asajgule@gmail.com>
2024-05-09 23:57:07 +0530

    format phone number problem solved

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,5 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(num) {
+    return `(${num.slice(0, 3).join("")}) ${num.slice(3, 6).join("")}-${num.slice(6).join("")}`
+}
 
 module.exports = formatPhoneNumber;
